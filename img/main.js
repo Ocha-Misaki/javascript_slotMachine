9d667f3c7206c61ec45c6f8c62fb2a95466e8652
misamisa <itoumisaki@misamisaMacBook-Air.local>
2023-08-17 13:17:20 +0900

    関数createElementを変更

--- img/main.js
+++ img/main.js
@@ -1,25 +1,29 @@
 'use strict'
 {
 
-//htmlを描画する
-//関数化したもの
-const createElement = (element) => {
-  const divElement = document.createElement('div')
-  divElement.classList.add('slot-panel')
-  const img = document.createElement('img')
-  img.src = 'cherry.png'
-  const stop = document.createElement('button')
-  stop.setAttribute('id',element)
-  stop.textContent = 'STOP'
-  divElement.appendChild(img)
-  divElement.appendChild(stop)
-  const slotBodyElement = document.querySelector('div')
-  slotBodyElement.appendChild(divElement)
-}
+  const createElement = (imgID,stopID) => {
+    const divElement = document.createElement('div')
+    divElement.classList.add('slot-panel')
 
-createElement('stop1')
-createElement('stop2')
-createElement('stop3')
+    const img = document.createElement('img')
+    img.src = 'seven.png'
+    img.setAttribute('id',imgID)
+    
+    const stop = document.createElement('button')
+    stop.setAttribute('id',stopID)
+    stop.textContent = 'STOP'
+    
+    divElement.appendChild(img)
+    divElement.appendChild(stop)
+    
+    const slotBodyElement = document.querySelector('div')
+    slotBodyElement.appendChild(divElement)
+  }
+
+  createElement('img1','stop1')
+  createElement('img2','stop2')
+  createElement('img3','stop3')
+
 
   //画像を切り替える
   const switchImage = () => {
@@ -31,34 +35,41 @@ createElement('stop3')
     return imgArray[Math.floor(Math.random() * imgArray.length)]
   }
 
+
+  const img1 = document.getElementById('img1')
+  const img2 = document.getElementById('img2')
+  const img3 = document.getElementById('img3')
+  
   let timeoutID1;
   let timeoutID2;
   let timeoutID3;
   
-  const spinningSlot = (timeoutID) => {
+  const spinningSlot = (timeoutID,img) => {
     timeoutID = setInterval(() => {
       img.src = switchImage()
     },10)
   }
 
-  const spin = document.getElementById('spin')
-  spin.addEventListener('click', () => {
-    spinningSlot(timeoutID1)
-    spinningSlot(timeoutID2)
-    spinningSlot(timeoutID3)
-  })
 
-  const stoppingSlot = (element, timeoutID) => {
-    element.addEventListener('click',() => {
-    clearInterval(timeoutID)
-    })
-  }
-  const stop1 = document.getElementById('stop1')
-  stoppingSlot(stop1,timeoutID1)
-  const stop2Element = document.getElementById('stop2')
-  stoppingSlot(stop2Element,timeoutID2)
-  const stop3Element = document.getElementById('stop3')
-  stoppingSlot(stop3Element,timeoutID3)
+
+const spin = document.getElementById('spin')
+spin.addEventListener('click', () => {
+  spinningSlot(timeoutID1,img1)
+  spinningSlot(timeoutID2,img2)
+  spinningSlot(timeoutID3,img3)
+})
+
+const stoppingSlot = (element,timeoutID) => {
+  element.addEventListener('click',() => {
+  clearInterval(timeoutID)
+  })
+}
+const stop1 = document.getElementById('stop1')
+const stop2 = document.getElementById('stop2')
+const stop3 = document.getElementById('stop3')
+stoppingSlot(stop1,timeoutID1)
+stoppingSlot(stop2,timeoutID2)
+stoppingSlot(stop3,timeoutID3)
 
 }
 
--- img/main_sub.js
+++ img/main_sub.js
@@ -15,38 +15,41 @@ timeoutID = setInterval(() => {
 
 
 // //一つ目のスロット
-  // const divElement1 = document.createElement('div')
-  // divElement1.classList.add('slot-panel')
-  // const img1 = document.createElement('img')
-  // img1.src = 'cherry.png'
-  // const stop1 = document.createElement('button')
-  // stop1.setAttribute('id','stop1')
-  // stop1.textContent = 'STOP'
-  // divElement1.appendChild(img1)
-  // divElement1.appendChild(stop1)
-  // const slotBodyElement = document.querySelector('div')
-  // slotBodyElement.appendChild(divElement1)
+  const divElement1 = document.createElement('div')
+  divElement1.classList.add('slot-panel')
+  const img1 = document.createElement('img')
+  img1.src = 'cherry.png'
+  const stop1 = document.createElement('button')
+  stop1.setAttribute('id','stop1')
+  stop1.textContent = 'STOP'
+  divElement1.appendChild(img1)
+  divElement1.appendChild(stop1)
+  const slotBodyElement = document.querySelector('div')
+  slotBodyElement.appendChild(divElement1)
 
   // //二つ目
-  // const divElement2 = document.createElement('div')
-  // divElement2.classList.add('slot-panel')
-  // const img2 = document.createElement('img')
-  // img2.src = 'cherry.png'
-  // const stop2 = document.createElement('button')
-  // stop2.setAttribute('id','stop2')
-  // stop2.textContent = 'STOP'
-  // divElement2.appendChild(img2)
-  // divElement2.appendChild(stop2)
-  // slotBodyElement.appendChild(divElement2)
+  const divElement2 = document.createElement('div')
+  divElement2.classList.add('slot-panel')
+  const img2 = document.createElement('img')
+  img2.src = 'cherry.png'
+  const stop2 = document.createElement('button')
+  stop2.setAttribute('id','stop2')
+  stop2.textContent = 'STOP'
+  divElement2.appendChild(img2)
+  divElement2.appendChild(stop2)
+  slotBodyElement.appendChild(divElement2)
   
   // //三つ目
-  // const divElement3 = document.createElement('div')
-  // divElement3.classList.add('slot-panel')
-  // const img3 = document.createElement('img')
-  // img3.src = 'cherry.png'
-  // const stop3 = document.createElement('button')
-  // stop3.setAttribute('id','stop3')
-  // stop3.textContent = 'STOP'
-  // divElement3.appendChild(img3)
-  // divElement3.appendChild(stop3)
-  // slotBodyElement.appendChild(divElement3)
+  const divElement3 = document.createElement('div')
+  divElement3.classList.add('slot-panel')
+  const img3 = document.createElement('img')
+  img3.src = 'cherry.png'
+  const stop3 = document.createElement('button')
+  stop3.setAttribute('id','stop3')
+  stop3.textContent = 'STOP'
+  divElement3.appendChild(img3)
+  divElement3.appendChild(stop3)
+  slotBodyElement.appendChild(divElement3)
+
+
+  
