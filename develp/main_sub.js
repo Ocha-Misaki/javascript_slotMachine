2dc4ac5975726175702ff7911eb4dabb682499a5
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-08-27 10:58:45 +0900

    dotinstall版完成

--- develp/main_sub.js
+++ develp/main_sub.js
@@ -7,17 +7,59 @@
       section.classList.add('panel')
 
       this.img = document.createElement('img')
-      this.img.src = 'img/seven.png'
+      this.img.src = this.getRandomImg()
+
+      this.timeoutId = undefined
 
       this.stop = document.createElement('div')
       this.stop.textContent = 'STOP'
-      this.stop.classList.add('stop')
-      
+      this.stop.classList.add('stop','inactive')
+      this.stop.addEventListener('click',() => {
+        if(this.stop.classList.contains('inactive')){
+          return
+        }
+        this.stop.classList.add('inactive')
+        clearTimeout(this.timeoutId)
+        panelsLeft--
+        if(panelsLeft === 0){
+          spin.classList.remove('inactive')
+          panelsLeft = 3
+          checkResult()
+        }
+      })
       section.appendChild(this.img)
       section.appendChild(this.stop)
 
       const main = document.querySelector('main')
       main.appendChild(section)
+    }
+
+    spin(){
+      this.img.src = this.getRandomImg()
+      this.timeoutId = setTimeout(() => {
+        this.spin()
+      },10)
+    }
+    getRandomImg(){
+      const images = [
+        'img/seven.png',
+        'img/cherry.png',
+        'img/bell.png',
+      ]
+      return images[Math.floor(Math.random() * images.length)]
+    }
+
+    isUnmatched(p1,p2){
+      return this.img.src !== p1.img.src && this.img.src !== p2.img.src
+    }
+
+    unMatch(){
+      this.img.classList.add('unmatched')
+    }
+
+    activate(){
+      this.img.classList.remove('unmatched')
+      this.stop.classList.remove('inactive')
     }
   }
   
@@ -27,4 +69,28 @@
     new Panel(),
   ]
 
+  let panelsLeft = 3
+
+  const checkResult = () => {
+    if(panels[0].isUnmatched(panels[1],panels[2])){
+      panels[0].unMatch()
+    }
+    if(panels[1].isUnmatched(panels[0],panels[2])){
+      panels[1].unMatch()
+    }
+    if(panels[2].isUnmatched(panels[0],panels[1])){
+      panels[2].unMatch()
+    }
+  }
+  const spin = document.getElementById('spin')
+  spin.addEventListener('click', () => {
+    if(spin.classList.contains('inactive')){
+      return
+    }
+    spin.classList.add('inactive')
+    panels.forEach(panel => {
+      panel.activate()
+      panel.spin()
+    })
+  })
 }
